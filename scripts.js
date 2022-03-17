e2645d59f16327af651aaba743ac7e69615bc540
AhoaT <ahoa.tran@gmail.com>
2022-03-17 10:49:14 +0000

    added get repos

--- scripts.js
+++ scripts.js
@@ -1,8 +1,20 @@
+let user = "ahoat";
+
 function getUser(user) {
     return fetch(`https://api.github.com/users/${user}`)
          .then((result) => result.json());
       };
 
-getUser("ahoat")
+getUser(user)
     .then((user) => console.log(user))
     .catch((error) => console.log(error));
+
+function getrepos(user) {
+    return fetch(`https://api.github.com/users/${user}/repos`)
+        .then((result) => result.json());
+          };
+
+getrepos(user)
+    .then((user) => console.log(user))
+    .catch((error) => console.log(error));
+    
