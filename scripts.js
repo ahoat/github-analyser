212d86e3bb2070f3613644ca13112f702d8105c0
AhoaT <ahoa.tran@gmail.com>
2022-03-27 02:39:36 +0100

    created headling data and CSS animation

--- scripts.js
+++ scripts.js
@@ -1,26 +1,70 @@
 const form = document.querySelector("form");
 const searchBtn = document.querySelector("#search-btn");
 const userInput = document.querySelector(".search-box");
+const headlineData = document.querySelector(".headline-data");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const username = userInput.value;
-    getUser(username)
-     .then((username) => console.log(username))
-     .catch((error) => console.log(error));
-    console.log(username);
-    getrepos(username)
-    .then((username) => console.log(username))
-    .catch((error) => console.log(error))
-
+    const userURL = `https://api.github.com/users/${username}`;
+    headlines(userURL);
+   // getUser(userURL);
+    getrepos(userURL);
+    //getAvatar(username);
+    // console.log(username.followers.value);
+      
 });
 
-function getUser(user) {
-    return fetch(`https://api.github.com/users/${user}`)
-         .then((result) => result.json());
-      };
+async function fetchData(user) {
+    
+        const response = await fetch(user);
+        const data = await response.json();
+    
+        if (!response.ok) {
+            throw new Error(`User not found: ${response.status} ${data.message}`);
+        } else {
+        return data;
+         }
+    }
 
-function getrepos(user) {
-    return fetch(`https://api.github.com/users/${user}/repos`)
-        .then((result) => result.json());
-          };        
+
+function createPElement(){
+    let p = document.createElement("p");
+    p.classList.add("userInfo")
+    return p;
+}
+
+async function headlines (user) {
+    const userData = await fetchData(`${user}`);
+    console.log(userData);
+    let avatar = document.createElement("img");
+    avatar.src = userData.avatar_url
+    let userName = createPElement();
+    userName.appendChild(document.createTextNode(`Username : ${userInput.value}`))
+    let location = createPElement();
+    location.appendChild(document.createTextNode(`Location : ${userData.location}`));
+    let followersNum = createPElement();
+    followersNum.appendChild(document.createTextNode(`Followers : ${userData.followers}`));
+    let followingNum = createPElement();
+    followingNum.appendChild(document.createTextNode(`Following : ${userData.following}`));
+    headlineData.appendChild(avatar);
+    headlineData.appendChild(userName);
+    headlineData.appendChild(location);
+    headlineData.appendChild(followersNum);
+    headlineData.appendChild(followingNum);
+}
+
+async function getrepos(user) {
+    const repos = await fetchData(`${user}/repos`);
+    console.log(repos);
+       
+          };
+
+
+//need to figure out how to get the user's avatar.
+function getAvatar(user) {
+    return fetch(`https://avatars.githubusercontent.com/u/${user.id}?v=4`)
+            .then((result) => result.json())
+            .then((user) => console.log(user))
+            .catch((error) => console.log(error))
+              };
